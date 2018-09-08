e23499e63522f634cdd28733a5ce6e9120900720
ina-uzu <ina.uzu.song@gmail.com>
2018-09-08 11:34:40 +0900

    edit RecordUrine page

--- app/src/main/java/com/example/medi/mediproject/RecordUrineActivity.java
+++ app/src/main/java/com/example/medi/mediproject/RecordUrineActivity.java
@@ -3,15 +3,22 @@ package com.example.medi.mediproject;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,220 +28,208 @@ import com.felhr.usbserial.UsbSerialDevice;
 import com.felhr.usbserial.UsbSerialInterface;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class RecordUrineActivity extends BaseActivity {
-    public final String ACTION_USB_PERMISSION = "com.hariharan.arduinousb.USB_PERMISSION";
-    Button startButton,stopButton;
-    TextView textView, tmp;
-    UsbManager usbManager;
-    UsbDevice device;
-    UsbSerialDevice serialPort;
-    UsbDeviceConnection connection;
-    String res=null;
-    String pid,name, time;
-    boolean first=false;
-
-    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
-        @Override
-        public void onReceivedData(byte[] arg0) {
-            String data = null;
-            try {
-                data = new String(arg0, "UTF-8");
-
-                if( (res==null&& !first) && (data!=null && data !="")){
-                    res=data;
-                    textView.setText(res+".0");
-                    first=true;
-                }
-
-                data.concat("/n");
-                tvAppend(tmp, data);
-
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-
-        }
-    };
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
+    boolean connected = false;
+    static EditText print_weight;
+    String weight="";
+    /*
+     * Notifications from UsbService will be received here.
+     */
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
-                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
-                if (granted) {
-                    connection = usbManager.openDevice(device);
-                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
-                    if (serialPort != null) {
+            switch (intent.getAction()) {
+                case UsbService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
+                    Toast.makeText(context, "측정 버튼을 눌러주세요", Toast.LENGTH_SHORT).show();
+                    connected=true;
+                    break;
 
-                        if (serialPort.open() ) { //Set Serial Connection Parameters.
-                            setUiEnabled(true);
-                            serialPort.setBaudRate(9600);
-                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
-                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
-                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
-                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                            serialPort.read(mCallback);
-
-                        } else {
-                            Log.d("SERIAL", "PORT NOT OPEN");
-                        }
-                    } else {
-                        Log.d("SERIAL", "PORT IS NULL");
-                    }
-                } else {
-                    Log.d("SERIAL", "PERM NOT GRANTED");
-                }
-            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
-                onClickStart(startButton);
-            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
-                onClickStop(stopButton);
-            }
-        }
-
-        ;
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_record_urine);
-        usbManager = (UsbManager) getSystemService(this.USB_SERVICE);
-
-        stopButton = (Button)findViewById(R.id.bt_stop);
-        startButton = (Button) findViewById(R.id.askWeight);
-        textView = (TextView) findViewById(R.id.weightPrint);
-        tmp = findViewById(R.id.urineUnit);
-
-        Intent intent =getIntent();
-        pid=intent.getStringExtra("pid");
-        name= MediValues.patientData.get(pid).get("name");
-        time ="";
-
-        buttonPrev = findViewById(R.id.Bnt_prev);
-        buttonNext =findViewById(R.id.Bnt_next);
-
-        setUiEnabled(false);
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(ACTION_USB_PERMISSION);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-        registerReceiver(broadcastReceiver, filter);
-
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                res=null;
-                first=false;
-
-                Toast.makeText(getApplicationContext(),"무게를 측정하였습니다",Toast.LENGTH_SHORT).show();
-                HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
-
-                if (!usbDevices.isEmpty()) {
-                    boolean keep = true;
-                    for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
-                        device = entry.getValue();
-                        int deviceVID = device.getVendorId();
-                        if (deviceVID == 0x2341)//Arduino Vendor ID
-                        {
-                            PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(ACTION_USB_PERMISSION), 0);
-                            usbManager.requestPermission(device, pi);
-                            keep = false;
-                        } else {
-                            connection = null;
-                            device = null;
-                        }
-                        if (!keep)
-                            break;
-                    }
-                }
-
-            }
-        });
-
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                serialPort.close();
-                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
-                setUiEnabled(false);
-            }
-        });
-    }
-
-
-    public void onPrevClick(View view) {
-        Intent intent = new Intent(RecordUrineActivity.this, ContainerSelectActivity.class);
-        intent.putExtra("pid", pid);
-        startActivity(intent);
-    }
-
-    public void onNextClick(View view) {
-
-        if(!textView.getText().toString().equals("")) {
-            float amount = Float.parseFloat(res);
-            MediPostRequest postRequest = new MediPostRequest(view.getContext(), pid, name, MediValues.OUTPUT, MediValues.URINE, amount, time);
-            Intent intent = new Intent(RecordUrineActivity.this, ReportActivity.class);
-            intent.putExtra("pid", pid);
-            startActivity(intent);
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "무게를 측정해 주세요", Toast.LENGTH_LONG).show();
-        }
-    }
-    public void setUiEnabled(boolean bool) {
-        startButton.setEnabled(!bool);
-        stopButton.setEnabled(bool);
-        textView.setEnabled(bool);
-    }
-
-    public void onClickStart(View view) {
-        Toast.makeText(getApplicationContext(),"측정 준비가 되었습니다",Toast.LENGTH_LONG).show();
-        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
-
-        if (!usbDevices.isEmpty()) {
-            boolean keep = true;
-            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
-                device = entry.getValue();
-                int deviceVID = device.getVendorId();
-                if (deviceVID == 0x2341)//Arduino Vendor ID
-                {
-                    PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-                    usbManager.requestPermission(device, pi);
-                    keep = false;
-                } else {
-                    connection = null;
-                    device = null;
-                }
-                if (!keep)
+                case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
+                    connected=false;
+                    Toast.makeText(context, "저울과 연결이 필요합니다", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_NO_USB: // NO USB CONNECTED
+                    connected=false;
+                    Toast.makeText(context, "저울과 연결이 필요합니다", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
+                    connected=false;
+                    Toast.makeText(context, "저울과 연결이 필요합니다", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
+                    connected=false;
+                    Toast.makeText(context, "저울과 연결이 필요합니다", Toast.LENGTH_SHORT).show();
                     break;
             }
         }
+    };
+    private UsbService usbService;
+    private MyHandler mHandler;
+
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder) arg1).getService();
+            usbService.setHandler(mHandler);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            usbService = null;
+        }
+    };
 
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        immersiveMode();
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        immersiveMode();
+                    }
+                });
     }
 
-
-    public void onClickStop(View view) {
-        serialPort.close();
-        Toast.makeText(getApplicationContext(), "저울과 연결이 필요합니다", Toast.LENGTH_LONG).show();
-        setUiEnabled(false);
+    public void immersiveMode() {
+        final View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        immersiveMode();
+        setFilters();  // Start listening notifications from UsbService
+        startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
     }
 
-    private void tvAppend(TextView tv, final CharSequence text) {
-        final TextView ftv = tv;
-        final CharSequence ftext = text;
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mUsbReceiver);
+        unbindService(usbConnection);
+    }
 
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                //ftv.append(ftext);
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(this, service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
             }
+            startService(startService);
+        }
+        Intent bindingIntent = new Intent(this, service);
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
 
+    private void setFilters() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(UsbService.ACTION_NO_USB);
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        registerReceiver(mUsbReceiver, filter);
+    }
+
+    /*
+     * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
+     */
+    private static class MyHandler extends Handler {
+        private final WeakReference<RecordUrineActivity> mActivity;
+
+        public MyHandler(RecordUrineActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UsbService.MESSAGE_FROM_SERIAL_PORT:
+                    String data = (String) msg.obj;
+
+                    //mActivity.get().display.append(data);
+                    mActivity.get().print_weight.setText(data);
+                    break;
+            }
+        }
+    }
+
+
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_record_urine);
+
+        Intent intent = getIntent();
+        final String pid = intent.getStringExtra("pid");
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(uiOptions);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mHandler = new MyHandler(this);
+
+        Button bt_start = findViewById(R.id.askWeight);
+        print_weight =  findViewById(R.id.weightPrint);
+        Button bt_next = findViewById(R.id.Bnt_next);
+        Button bt_prev = findViewById(R.id.Bnt_prev);
+
+        //측정시작
+        bt_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!connected)
+                    Toast.makeText(getApplicationContext(),"저울과 연결이 필요합니다",Toast.LENGTH_SHORT).show();
+
+                else{
+                    String data="1'";
+                    if (usbService != null) { // if UsbService was correctly binded, Send data
+                        usbService.write(data.getBytes());
+                    }
+                }
+            }
+        });
+
+        bt_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tmp = (print_weight.getText().toString());
+                if (tmp == null || tmp == "" || Float.parseFloat(tmp) <= 0)
+                    Toast.makeText(getApplicationContext(), "잘못된 무게값입니다", Toast.LENGTH_SHORT).show();
+
+                else {
+                    Toast.makeText(getApplicationContext(),tmp + "g의 소변이 등록되었습니다", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RecordUrineActivity.this, ReportActivity.class);
+                    intent.putExtra("pid", pid);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        bt_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecordUrineActivity.this, ContainerSelectActivity.class);
+                intent.putExtra("pid", pid);
+                startActivity(intent);
+            }
         });
     }
-
 }
